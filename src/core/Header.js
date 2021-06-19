c0dc30b485fe606f17f4f5c0f2254cb3e0fdb08c
prakhyat-shrestha <erprakhyat@gmail.com>
2021-06-19 12:29:20 +0545

    search form done but skipped results display page

--- src/core/Header.js
+++ src/core/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import SearchBar from "./SearchBar";
 
 const Header = () => {
   return (
@@ -10,19 +11,7 @@ const Header = () => {
           <img src="../images/logo.svg" className="w-32 " alt="logo" />
         </Link>
         {/* search bar */}
-        <div className="w-full max-w-xl relative flex">
-          <span className="absolute left-4 top-3 text-lg text-gray-400">
-            <i className="fas fa-search" />
-          </span>
-          <input
-            type="text"
-            className="w-full border border-primary border-r-0 pl-12 py-3 pr-3 rounded-l-md focus:outline-none"
-            placeholder="search"
-          />
-          <button className="bg-primary border border-primary text-white px-8 rounded-r-md hover:bg-transparent hover:text-primary transition">
-            Search
-          </button>
-        </div>
+        <SearchBar />
         {/* icons */}
         <div className="flex items-center space-x-4">
           <Link
